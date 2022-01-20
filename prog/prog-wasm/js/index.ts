2216de0877e879e2e2b529173afed4ace461b582
Nils <nils@nilsand.re>
2022-01-20 23:37:24 +0000

    Remove unused code in index.ts

--- prog/prog-wasm/js/index.ts
+++ prog/prog-wasm/js/index.ts
@@ -2,15 +2,6 @@ import { run } from "prog-wasm";
 
 const form = document.getElementById("code-form");
 const textarea = <HTMLTextAreaElement>document.getElementById("code-textarea");
-const output = <HTMLOutputElement>document.getElementById("output");
-
-function clear_output() {
-  output.value = "";
-}
-
-function append_to_output(text: string) {
-    output.value += text;
-}
 
 form.addEventListener("submit", e => {
     e.preventDefault();
