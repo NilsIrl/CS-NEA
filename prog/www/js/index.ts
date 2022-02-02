ff16ca3258a8aac7aaabe448a174e38fe84117a3
Nils <nils@nilsand.re>
2022-02-02 15:40:39 +0000

    Terminate process on CTRL-C

--- prog/www/js/index.ts
+++ prog/www/js/index.ts
@@ -5,8 +5,16 @@ let term = new Terminal({
     convertEol: true,
     logLevel: "debug",
 });
+let scriptRunner;
 
 term.open(document.getElementById("terminal"));
+
+term.onKey((e: {key: string, domEvent: KeyboardEvent }) => {
+    const ev = e.domEvent;
+    if (ev.ctrlKey && ev.key == "c") {
+        scriptRunner.terminate();
+    }
+});
 
 const form = document.getElementById("code-form");
 const textarea = <HTMLTextAreaElement>document.getElementById("code-textarea");
@@ -14,7 +22,7 @@ const textarea = <HTMLTextAreaElement>document.getElementById("code-textarea");
 form.addEventListener("submit", e => {
     e.preventDefault();
     term.clear();
-    const scriptRunner = new Worker(
+    scriptRunner = new Worker(
         new URL('worker.ts', import.meta.url),
     );
     scriptRunner.onmessage = function(e) {
