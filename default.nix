baf4669bcc2a3859f71ae4f122de59cf16d31cbd
Nils <nils@nilsand.re>
2022-04-10 21:06:43 +0100

    Add rust dependencies to default.nix

--- default.nix
+++ default.nix
@@ -6,11 +6,17 @@ pkgs.mkShell {
   ];
 
   nativeBuildInputs = [
+    (pkgs.rust-bin.nightly."2022-04-10".default.override {
+      targets = [ "wasm32-unknown-unknown" ];
+    })
+    pkgs.binaryen
+    pkgs.gcc
     pkgs.python39Packages.pygments
     pkgs.biber
     pkgs.wasm-pack
     (pkgs.texlive.combine {
       inherit (pkgs.texlive) scheme-minimal latexmk newunicodechar babel import hyperref biblatex caption adjustbox makecell siunitx attachfile2 minted letltxmacro xkeyval collectbox pgf fvextra upquote lineno fancyvrb catchfile xstring framed float collection-latex;
     })
+    pkgs.nodejs-16_x
   ];
 }
